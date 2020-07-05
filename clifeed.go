d2c6678963fb1d9bbe699c0f740968cd3673b51e
berfr <berfr4@gmail.com>
2020-07-05 13:34:10 -0400

    Add support for reading feeds list

--- clifeed.go
+++ clifeed.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/mmcdole/gofeed"
 )
 
 func main() {
+	file, err := os.Open("feeds.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
 	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL("https://berfr.github.io/index.xml")
-	fmt.Println(feed.Title)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		feedURL := scanner.Text()
+		feed, _ := fp.ParseURL(feedURL)
+		fmt.Println(feed.Title)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
