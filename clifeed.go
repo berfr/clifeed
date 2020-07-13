c14dbadfdad608516f98c0bde2d3bd044cb0f57a
berfr <berfr4@gmail.com>
2020-07-12 22:08:19 -0400

    Add support for sorting, filtering and fetching concurrently

--- clifeed.go
+++ clifeed.go
@@ -5,18 +5,47 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"sync"
+	"time"
 
 	"github.com/mmcdole/gofeed"
 )
 
-func getFeed(feedURL string, wg *sync.WaitGroup) {
+type Item struct {
+	Date      time.Time
+	FeedTitle string
+	Title     string
+	URL       string
+}
+
+func (i Item) String() string {
+	return fmt.Sprintf("%s | %s | \033]8;;%s\a%s\033]8;;\a", i.Date.Format("2006-01-02"), i.FeedTitle, i.URL, i.Title)
+}
+
+func handleItems(ch chan Item, done chan bool) {
+	lastWeek := time.Now().AddDate(0, -1, 0)
+	var lastWeekItems []Item
+	for item := range ch {
+		if item.Date.After(lastWeek) {
+			lastWeekItems = append(lastWeekItems, item)
+		}
+	}
+	sort.Slice(lastWeekItems, func(i, j int) bool {
+		return lastWeekItems[i].Date.After(lastWeekItems[j].Date)
+	})
+	for _, item := range lastWeekItems {
+		fmt.Println(item.String())
+	}
+	done <- true
+}
+
+func getFeed(feedURL string, wg *sync.WaitGroup, ch chan Item) {
 	defer wg.Done()
 	fp := gofeed.NewParser()
 	feed, _ := fp.ParseURL(feedURL)
-	fmt.Println("title:", feed.Title)
 	for _, item := range feed.Items {
-		fmt.Println("item:", item.Title, item.Published)
+		ch <- Item{*item.PublishedParsed, feed.Title, item.Title, item.Link}
 	}
 }
 
@@ -29,14 +58,21 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	var wg sync.WaitGroup
+	ch := make(chan Item)
 
 	for scanner.Scan() {
 		feedURL := scanner.Text()
 		wg.Add(1)
-		go getFeed(feedURL, &wg)
+		go getFeed(feedURL, &wg, ch)
 	}
 
+	done := make(chan bool)
+	go handleItems(ch, done)
+
 	wg.Wait()
+	close(ch)
+	<-done
+	close(done)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
